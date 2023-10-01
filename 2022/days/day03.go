f3889de1ee91f42ef063ccc540f1d04aa7a20440
Jonathan <virtualnevertheless@gmail.com>
2023-10-01 17:26:50 -0400

    day03 complete

--- 2022/days/day03.go
+++ 2022/days/day03.go
@@ -54,12 +54,59 @@ func sumCompartments() {
   fmt.Printf("the sum of the priorities of those item types is %d\n", totalPriorities)
 }
 
+func sumGroupsOfThree() {
+  file, err := os.Open("./inputs/input03.txt")
+  defer file.Close() 
+
+  if err != nil {
+    fmt.Println(err)
+  }
+  
+  totalPriorities := 0
+  fileReader := bufio.NewReader(file)
+
+  for {
+    var elf1, elf2, elf3 []byte 
+    _, err := fmt.Fscanf(fileReader, "%s\n%s\n%s\n", &elf1, &elf2, &elf3)
+
+    if err != nil {
+      break
+    }
+
+    itemFrequency := make(map[byte]int)
+
+    for _, char := range elf1 {
+      itemFrequency[char] = 1
+    }
+
+    for _, char := range elf2 {
+      _, ok := itemFrequency[char]
+
+      if ok {
+        itemFrequency[char] = 2
+      }
+    }
+
+    for _, char := range elf3 {
+      item, ok := itemFrequency[char]
+
+      if ok && item == 2 {
+        totalPriorities += itemPrioity(char)
+        break
+      }
+    }
+  }
+
+  fmt.Printf("part 2's sum of the priorities of those item types is %d.\n", totalPriorities)
+
+}
+
 func Day03(part string) {
   switch part {
   case "1":
     sumCompartments()
   case "2":
-    fmt.Println("not implemented yet.")
+    sumGroupsOfThree()
   default:
     fmt.Println("invalid part provided.") 
   }
